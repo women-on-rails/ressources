18de68cbdf12fbfab0f710c40b13d456ea428ca3
Juliette Audema <juliette@capsens.eu>
2020-10-16 13:17:45 +0200

    Customize Infima classes for avatar

--- website/src/components/horizontal_card.js
+++ website/src/components/horizontal_card.js
@@ -9,14 +9,14 @@ export default function HorizontalCard({
   description
   }) {
   return (
-    <div class="avatar mt-1">
+    <div class="avatar">
       <a href={href} target="_blank" rel="noopener noreferrer">
         <img
           class="avatar__photo avatar__photo--xl"
           src={useBaseUrl(src)}
         />
       </a>
-      <div class="avatar__intro ml-1">
+      <div class="avatar__intro">
         <h4 class="avatar__name">
           <a href={href} target="_blank" rel="noopener noreferrer">{title}</a>
         </h4>
