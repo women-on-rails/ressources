73ea9a8f9f0dbdfe0e0f410259acdd22aa83da70
Clodiap <clodiapp@gmail.com>
2020-10-25 17:48:03 +0100

    Add space into brackets for lisibility

--- website/src/components/timeline_step.js
+++ website/src/components/timeline_step.js
@@ -3,9 +3,9 @@ import styles from '@site/src/css/timeline.module.css';
 
 export default function TimelineStep (props){
   return (
-    <div className={styles.step}>
-      <h2>{props.title}</h2>
-      <p>{props.children}</p>
+    <div className={ styles.step }>
+      <h2>{ props.title }</h2>
+      <p>{ props.children }</p>
     </div>
   );
 }
