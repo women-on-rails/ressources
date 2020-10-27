a68a01bbe9f05a992218254a81bbf1641223102f
morganbonhomme <bonhomme.morgan@gmail.com>
2020-10-27 10:57:23 +0100

    updated dark-mode

--- website/src/components/blog_card.js
+++ website/src/components/blog_card.js
@@ -5,12 +5,9 @@ import useThemeContext from "@theme/hooks/useThemeContext";
 export default function BlogCard(props) {
   const { isDarkTheme, setLightTheme, setDarkTheme } = useThemeContext();
 
-  let style = "";
-  isDarkTheme ? (style = "--dark-mode") : (style = "");
-
   return (
     <a className="card__link" href={props.link}>
-      <div className={`card card${style}`}>
+      <div className="card">
         <div className="card__image">
           <img
             src={props.image}
@@ -23,10 +20,8 @@ export default function BlogCard(props) {
           <span className="card__tag">{props.tag}</span>
         </div>
         <div className="card__body">
-          <h3 className={`text--bold text${style}`}>{props.title}</h3>
-          <p className={`text--left text--summary text${style}`}>
-            {props.summary}
-          </p>
+          <h3 className="text--bold">{props.title}</h3>
+          <p className="text--left text--summary">{props.summary}</p>
         </div>
         <div className="card__footer">
           <p className="card__author">par {props.author}</p>
