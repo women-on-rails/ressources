e087dbb22896e4ba900f2c86857943c8805870fb
Juliette Audema <juliette@capsens.eu>
2020-11-21 20:40:51 +0100

    Finish up config build:fr

--- website/build.fr.sh
+++ website/build.fr.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-echo hello
+cd website
+yarn install
+yarn build
+mv build /tmp/docusaurus-build
+cd ..
+git checkout gh-pages
+rm -rf website
+mv /tmp/docusaurus-build/* ./
+for FILE in $(find . -iname '*.html'); do
+  sed -i '' 's/lang="en"/lang="fr"/g' $FILE
+done
+ga .
+git commit -m "Build $(date +"%D %T")"
+git push
