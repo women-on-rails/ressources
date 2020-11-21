0c6890573eda1fbddda9eb5175b63051c391cbd3
Juliette Audema <juliette@capsens.eu>
2020-11-21 21:15:48 +0100

    Build 11/21/20 21:15:48

---
+++ 606836b4.6919131f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{75:function(e){e.exports=JSON.parse('{"permalink":"/ressources/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"previousPage":null,"nextPage":null,"blogDescription":"Blog"}')}}]);
