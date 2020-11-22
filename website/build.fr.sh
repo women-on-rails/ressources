cebd48fe2b9fea5cf05d7360d26d006ac6bb9811
Juliette Audema <juliette@capsens.eu>
2020-11-22 18:46:23 +0100

    Fix build:fr because it doesn't have alias

--- website/build.fr.sh
+++ website/build.fr.sh
@@ -8,7 +8,7 @@ cd ..
 git checkout gh-pages
 rm -rf *
 y
-ga .
+git add .
 git commit -m "Clean branch"
 mv /tmp/docusaurus-build/* ./
 mv build/* ./
@@ -16,6 +16,6 @@ rm -rf build
 for FILE in $(find . -iname '*.html'); do
   sed -i '' 's/lang="en"/lang="fr"/g' $FILE
 done
-ga .
+git add .
 git commit -m "Build $(date +"%D %T")"
 git push
