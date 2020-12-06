b131ccab73d4c399809c6e2d8c62d36fbe8e6bdb
Clodiap <clodiapp@gmail.com>
2020-12-06 15:47:04 +0100

    Add meeting_card component

---
+++ website/src/components/meeting_card.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+export default function MeetingCard({
+  src,
+  title,
+  href,
+  description,
+  frequency
+  }) {
+  return (
+    <div style={{ marginBottom: '1rem' }}>
+      <div class="card" style={{ border: 'none' }}>
+      <div class="container">
+
+        <div class = "row">
+          <div class="col col--3">
+            <a href={href} target="_blank" rel="noopener noreferrer">
+              <img
+                class="avatar__photo avatar__photo--xl"
+                src={useBaseUrl(src)}
+                alt={title}
+              />
+            </a>
+          </div>
+          <div class="col col--9">
+            <div class="card__header">
+              <a href={href} target="_blank" rel="noopener noreferrer">
+                <h3>{title}</h3>
+              </a>
+              <a class="button button--primary" href={href}>
+                {frequency}
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class = "row">
+          <div class="card__body">
+            <p>{description}</p>
+          </div>
+        </div>
+
+      </div>
+      </div>
+    </div>
+  );
+}
