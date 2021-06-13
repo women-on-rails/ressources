81266fa8024f5cbb94d65d92c85453aa5bb92afe
Juliette Audema <juliette@capsens.eu>
2021-06-13 20:38:59 +0200

    Add page on produce content

---
+++ website/docs/practice/produce-content.md
@@ -0,0 +1,18 @@
+---
+id: produce-content
+title: Produire du contenu
+slug: /produire-du-contenu
+---
+
+## Talks
+
+https://deniseyu.io/2018/09/24/writing-tech-talks.html
+
+Faire des belles slides: https://stephaniewalter.design/blog/create-better-conference-presentations-slides/
+
+https://github.com/slidevjs/slidev
+
+
+## Articles
+
+https://github.com/rubymorillo/pocket-tech-writing-list
--- website/src/sidebars.js
+++ website/src/sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
     Bienvenue: ['welcome/start', 'welcome/how-to-contribute'],
     'Faire son profil en ligne': ['online_profile/cv', 'online_profile/portfolio', 'online_profile/blog', 'online_profile/design-resources'],
     'Trouver un job': ['find_a_job/job-offers', 'find_a_job/hiring-process', 'find_a_job/interview-types', 'find_a_job/katas', 'find_a_job/technical-questions'],
-    Pratiquer: ['practice/side-projects', 'practice/open-source'],
+    Pratiquer: ['practice/side-projects', 'practice/open-source', 'practice/produce-content'],
     "Se former et s'informer": ['keeping_up/sites-and-tools', 'keeping_up/blogs-and-newsletters', 'keeping_up/books', 'keeping_up/videos-and-podcasts'],
     'La communauté RoR': ['community/socials', 'community/events'],
   }
