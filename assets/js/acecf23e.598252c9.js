76e468208db7dd1f99915fd4240f9f38a2570780
Juliette Audema <juliette.audema@gmail.com>
2024-08-14 15:26:22 +0200

    Deploy website - based on c885f11367a9caa8482959a1b525c05fe7cb773a

---
+++ assets/js/acecf23e.598252c9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/ressources/blog","blogTitle":"Blog","authorsListPath":"/ressources/blog/authors"}')}}]);
---
+++ assets/js/f8075bfc.93d5f1c0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[162],{6551:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/ressources/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
