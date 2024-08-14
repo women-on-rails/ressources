093fbd9b4e267b2ba3bd9a26deb725356324bf3d
Juliette Audema <juliette.audema@gmail.com>
2024-08-14 15:14:20 +0200

    Remove dark images because they are buggy in this version

--- website/src/components/dark_mode_image.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-
-export default function DarkModeImage(props) {
-  const {isDarkTheme, setLightTheme, setDarkTheme} = useThemeContext();
-
-  return <img src={isDarkTheme ? useBaseUrl(props.imgsrcdark) : useBaseUrl(props.imgsrclight)} class={props.customclass} />;
-}
